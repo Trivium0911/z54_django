b28abdecd39d451da9e3d78cb347b66f20bf250a
Trivium0911 <triv_1993@mail.ru>
2021-09-10 17:24:41 +0300

    Trying_z54 starts with django

---
+++ z54_django/superproject/project/urls.py
@@ -0,0 +1,15 @@
+
+from django.contrib import admin
+from django.http import HttpResponse, HttpRequest
+from django.urls import path
+
+
+def hello_world(request: HttpRequest):
+    x = str(request.headers)
+    return HttpResponse(x)
+
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+path('hw/', hello_world),
+]
