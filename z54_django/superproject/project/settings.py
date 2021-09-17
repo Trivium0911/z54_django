d4ca83fd581a6000790c3c0e74d1f6448998a8f2
Trivium0911 <triv_1993@mail.ru>
2021-09-17 18:57:51 +0300

    added procfile , heroku-django

--- z54_django/superproject/project/settings.py
+++ z54_django/superproject/project/settings.py
@@ -109,6 +109,8 @@ STATIC_URL = '/static/'
 
 if not DEBUG:
     STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+
+
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
@@ -126,3 +128,5 @@ CORS_ALLOW_HEADERS = [
     "x-requested-with",
     "x-user",
 ]
+
+django_heroku.settings(locals())
