dd6aa75fdb72a0bcb9069bc83244b17130092cae
Trivium0911 <triv_1993@mail.ru>
2021-10-01 01:47:56 +0300

    Added "author" to database (migrate)

--- superproject/blog/models.py
+++ superproject/blog/models.py
@@ -1,14 +1,17 @@
+from django.contrib.auth import get_user_model
 from django.db import models
+from django.urls import reverse_lazy
 
-
+User = get_user_model()
 class Post(models.Model):
     title = models.TextField()
     content = models.TextField(blank=True, null = True)
     hidden = models.BooleanField(default= False)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
 
     def get_absolute_url(self)->str:
-        return f"/blog/{self.pk}/"
+        return reverse_lazy("blog:single", kwargs={"pk" : self.pk})
 
     def __repr__(self):
         return f"{'🤫' if self.hidden else '👀'}{self.__class__.__name__}(id={self.pk}, title = {self.title!r})"
